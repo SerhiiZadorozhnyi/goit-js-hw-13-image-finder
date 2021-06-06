1f49e451d238d7e9fb72970ee814fd681dcb5f6e
SerhiiZadorozhnyi <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-06-07 00:06:40 +0300

    update key

--- src/js/constans.js
+++ src/js/constans.js
@@ -1,2 +1,2 @@
 export const BASE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal`;
-export const API_KEY = `21863327-3a0c3665fbb741a5e40073ebf`;
+export const API_KEY = `21964981-55d7d75076a0e2608b630ef4e`;
